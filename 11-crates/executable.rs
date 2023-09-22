67c47236d80d5a088fce819aa6ebdf2504a619fd
Benson Fan <bensonfan@hotmail.com>
2023-09-22 12:13:59 -0700

    add chapter 11 and 12

---
+++ 11-crates/executable.rs
@@ -0,0 +1,20 @@
+/* To use the library
+
+# Where library.rlib is the path to the compiled library, assumed that it's
+# in the same directory here:
+$ rustc executable.rs --extern rary=library.rlib && ./executable 
+called rary's `public_function()`
+called rary's `indirect_access()`, that
+> called rary's `private_function()`
+*/
+
+// extern crate rary; // May be required for Rust 2015 edition or earlier
+
+fn main() {
+    rary::public_function();
+
+    // Error! `private_function` is private
+    //rary::private_function();
+
+    rary::indirect_access();
+}
