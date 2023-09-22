7a88d2d6034150d6bd52faa44c3c488e9d0112de
Benson Fan <bensonfan@hotmail.com>
2023-09-22 13:45:24 -0700

    add charpter 13

---
+++ 13-attributes/cfg/src/custom.rs
@@ -0,0 +1,12 @@
+/* to test the custom cfg
+rustc --cfg some_condition custom.rs && ./custom
+*/
+
+#[cfg(some_condition)]
+fn conditional_function() {
+    println!("condition met!");
+}
+
+fn main() {
+    conditional_function();
+}
